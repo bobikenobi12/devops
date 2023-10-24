cd207e5209af6c47852e154ec3aad52676511bb6
bobikenobi12 <borislav1207@gmail.com>
2023-10-24 21:31:52 +0300

    add config for easyrsa

---
+++ second/config-default.sh
@@ -0,0 +1,8 @@
+KEY_COUNTRY="US"
+KEY_PROVINCE="CA"
+KEY_CITY="SanFrancisco"
+KEY_ORG="Fort-Funston"
+KEY_EMAIL="me@myhost.mydomain"
+KEY_OU="MyOrganizationalUnit"
+
+PUBLIC_IP=$(curl -s ifconfig.me)
